d01d5a1ccfeb13efd21581d7071b22a0478254c4
smelcerc <chris.smelcer@ldschurch.org>
2021-04-26 09:40:39 -0600

    updated team assignment

--- CS-241/team02.py
+++ CS-241/team02.py
@@ -2,21 +2,27 @@ def prompt_filename():
     file_name = input('What is your file? ')
     return file_name
 
-def parse_file(file_name):
+def prompt_search_term():
+    search_term = input('What word/term would you like to count? ')
+    return search_term.lower()
+
+def parse_file(file_name, search_term):
     count = 0
     with open(file_name, 'r') as text_file:
         for line in text_file:
             words = line.split()
             for word in words:
-                if word == 'pride' or word == 'Pride':
+                word_exist = word.lower().find(search_term)
+                if word_exist != -1:
                     count += 1
     return count
 
 def main():
     file_name = prompt_filename()
-    pride_count = parse_file(file_name)
+    search_term = prompt_search_term()
+    word_count = parse_file(file_name, search_term)
 
-    print(pride_count)
+    print(word_count)
 
 if __name__ == '__main__':
     main()
